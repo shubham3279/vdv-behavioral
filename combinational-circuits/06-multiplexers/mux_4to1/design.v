507a7e26bd4d1b645e460b2805ea6a5a4af7a7bc
shubhamv0918 <shubhamv0918@gmail.com>
2024-12-29 11:14:01 +0000

    added mux_4to1 design

---
+++ combinational-circuits/06-multiplexers/mux_4to1/design.v
@@ -0,0 +1,20 @@
+module mux_4to1 (
+    input din0, din1, din2, din3,
+    input reg [1:0] sel,
+    output reg dout
+);
+    
+    // combinational modelling
+    always @(* )begin
+        if (sel == 2'b00)
+            dout = din1;
+        else if (sel == 2'b01):
+            dout = din1;
+        else if (sel == 2'b01):
+            dout = din2;
+        else
+            dout = din3;
+    end
+
+endmodule
+
