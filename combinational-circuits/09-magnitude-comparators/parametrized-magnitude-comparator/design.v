b3bfe9fc6cd6ad460f79982177103cdb39262250
shubhamv0918 <shubhamv0918@gmail.com>
2024-12-30 18:59:20 +0000

    minor change

--- combinational-circuits/09-magnitude-comparators/parametrized-magnitude-comparator/design.v
+++ combinational-circuits/09-magnitude-comparators/parametrized-magnitude-comparator/design.v
@@ -1,12 +1,11 @@
+// this modelling is based on the logical of expressions of n-bit magnitude comprartor
+
 module mag_comp #(
     N = 4
 ) (
     input [N-1:0] a,b,
     output reg e,g,l
 );
-    e = 1;
-
-
     reg [N-1:0] exnor;
     reg [N-1:0] prod_b_comp;
     reg [N-1:0] prod_a_comp;
@@ -15,6 +14,7 @@ module mag_comp #(
     integer i;
     
     always @(*) begin
+
         for (i = 0; i < N; i = i + 1) begin
             exnor[i] = a[i] ^~ b[i];
             prod_b_comp[i] = a[i] & ~b[i];
@@ -25,14 +25,14 @@ module mag_comp #(
         
         
         g = prod_b_comp[N-1];
-        for (i = N-1; i >= 1; i = i - 1 ) begin
+        for (i = N-1; i > 0; i = i - 1 ) begin
             g =  g | ( exnor[i] & prod_b_comp[i-1] );
         end
 
         l = prod_a_comp[N-1];
-        for (i = N-1 ; i >= 1; i = i -1  ) begin
+        for (i = N-1 ; i > 0; i = i -1  ) begin
             l =  l | ( exnor[i] & prod_a_comp[i-1] );
         end
     end
-    
+
 endmodule
